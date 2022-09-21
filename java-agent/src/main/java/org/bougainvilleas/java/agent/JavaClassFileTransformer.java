a0a99f63a2be866f5efd6e33231526df7814cace
caddy <caddyren@qq.com>
2022-09-21 17:41:20 +0800

    test: 简单加密字节码文件

--- java-agent/src/main/java/org/bougainvilleas/java/agent/JavaClassFileTransformer.java
+++ java-agent/src/main/java/org/bougainvilleas/java/agent/JavaClassFileTransformer.java
@@ -7,16 +7,23 @@ import java.security.ProtectionDomain;
 public class JavaClassFileTransformer implements ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        if(className.startsWith("org/bougainvilleas/java/"))
+//        System.err.println(className);
+//        if(className.startsWith("com/example/demo/"))
+        if(className.contains("SimpleMetadataReader")||className.contains("ClassReader"))
+        {
+            System.err.println(loader.getClass().getName()+"___"+className);
+        }
+
+        if(className.contains("DemoApplication") || className.contains("DemoContraller"))
         {
             byte[] decode=new byte[classfileBuffer.length];
-            System.err.println(className);
+            System.err.println(loader.getClass().getName()+"___"+className);
 
             for (int i = 0; i < classfileBuffer.length; i++)
             {
-                System.err.print(Integer.toString(Byte.toUnsignedInt(classfileBuffer[i]),16));
+//                System.err.print(Integer.toString(Byte.toUnsignedInt(classfileBuffer[i]),16));
                 decode[i]= (byte) (classfileBuffer[i]^1);
-                System.err.print(Integer.toString(Byte.toUnsignedInt(decode[i]),16));
+//                System.err.print(Integer.toString(Byte.toUnsignedInt(decode[i]),16));
             }
             return decode;
         }else
